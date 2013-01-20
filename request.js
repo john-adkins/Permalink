d3ae7afe353c04f3b068e3e520be2691a0fb35ef
OpenShift guest <john@jadkins.co.uk>
2013-01-20 09:06:11 -0500

    asdsad

--- request.js
+++
@@ -1,7 +0,0 @@
-
-var requestMod = require('request');
-requestMod('http://www.google.com', function (e, r, b) {
-  if (!e && r.statusCode == 200) {
-    console.log(b) // Print the google web page.
-  }
-})
